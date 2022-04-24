33ee111907558323bead1ad014d73514bcc4e27a
A-F-K-hub <62518244+A-F-K-hub@users.noreply.github.com>
2022-04-24 19:05:58 +0200

    Add project files.

---
+++ BlazorChatApp/Server/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorChatApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await AddMessageToChat("", "User Connected!");
+            await base.OnConnectedAsync();
+        }
+        //Send msgs to all users and clients connected to hub
+        public async Task AddMessageToChat(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+    }
+}
